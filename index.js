aa906e07dd0e90f5e90768d296fe7dfee33cc0f4
codingIsNotMyPassion <venkatakrishnakoneti@gmail.com>
2022-04-13 19:09:47 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const productRoute = require("./routes/product");
+
+// const cors = require("cors");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/products", productRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
